0833cfcdd74200ef05068e117e26c25d887e6de3
Griffin Barnard <griffin.barnard@icloud.com>
2022-09-05 15:20:57 -0500

    first

---
+++ app/src/main/java/com/example/fastfinder/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.fastfinder;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("FasterFind");
+
+        jsonParse();
+    }
+
+
+    public void jsonParse() {
+
+        RequestQueue mQueue;
+
+        LinearLayout linearLayout = findViewById(R.id.explore);
+
+        mQueue = Volley.newRequestQueue(this);
+
+        String url = "https://api.foursquare.com/v3/places/search?limit=50";
+
+        JsonObjectRequest request =
+                new JsonObjectRequest(Request.Method.GET, url, null,
+                        response -> {
+                            try {
+                                JSONArray jsonArray = response.getJSONArray("results");
+
+                                for (int i = 0; i < jsonArray.length(); i++) {
+                                    JSONObject place = jsonArray.getJSONObject(i);
+
+                                    String name = place.getString("name");
+//                                    String distance = place.getString("distance");
+//                                    String img = place.getString("icon.prefix")+place.getString("icon.suffix");
+
+                                    LayoutInflater lf = getLayoutInflater();
+                                    View myLayout = lf.inflate(R.layout.layout, null);
+                                    TextView card = myLayout.findViewById(R.id.maintext);
+                                    card.setText(name);
+//                                    ImageView cardimg = myLayout.findViewById(R.id.cardimg);
+//                                    cardimg.setImageIcon(img);
+//                                    TextView othertxt = myLayout.findViewById(R.id.desctxt);
+//                                    othertxt.setText(distance);
+                                    linearLayout.addView(myLayout);
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }, Throwable::printStackTrace) {
+                    @Override
+                    public Map<String, String> getHeaders() {
+                        HashMap<String, String> customHeaders = new HashMap<>();
+
+                        customHeaders.put("Accept", "application/json");
+                        customHeaders.put("Authorization", "fsq35BlCYuH6Vsyan/JnNdJYxqtpXyJX06MekXcoYhb85qA=");
+
+                        return customHeaders;
+                    }
+                };
+        Log.d("findlopl", "text");
+        mQueue.add(request);
+    }
+}
